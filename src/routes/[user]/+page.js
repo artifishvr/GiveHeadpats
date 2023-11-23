99a8f18818d58f32c4df29c2065a5885f186c8a8
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-11-22 22:11:23 -0800

    try disabling ssr

--- src/routes/[user]/+page.js
+++ src/routes/[user]/+page.js
@@ -2,48 +2,45 @@ import { Query } from 'appwrite';
 import { databases } from '$lib/appwrite';
 import { env } from '$env/dynamic/public';
 
+export const ssr = false;
+
 export async function load({ params }) {
 
-	try {
-		const listdatabase = await databases.listDocuments(
-			env.PUBLIC_HEADPATDB,
-			env.PUBLIC_COLLECTION_HEADPATLIST,
-			[
-				Query.equal('headpatted', params.user),
-				Query.orderDesc("$createdAt"),
-				Query.limit(5),
-			]
-		);
+	const listdatabase = await databases.listDocuments(
+		env.PUBLIC_HEADPATDB,
+		env.PUBLIC_COLLECTION_HEADPATLIST,
+		[
+			Query.equal('headpatted', params.user),
+			Query.orderDesc("$createdAt"),
+			Query.limit(5),
+		]
+	);
 
-		const userdata = await databases.listDocuments(
-			env.PUBLIC_HEADPATDB,
-			env.PUBLIC_COLLECTION_USERDATA,
-			[
-				Query.equal('user', params.user),
-				Query.limit(1),
-			]
-		);
+	const userdata = await databases.listDocuments(
+		env.PUBLIC_HEADPATDB,
+		env.PUBLIC_COLLECTION_USERDATA,
+		[
+			Query.equal('user', params.user),
+			Query.limit(1),
+		]
+	);
 
-		if (userdata.total == 0) {
-			return {
-				headpats: 0,
-				user: "User not found",
-				allpats: [],
-				status: 404,
-			}
-		}
-
-
-
+	if (userdata.total == 0) {
 		return {
-			headpats: userdata.documents[0].PatsReceived,
-			user: params.user,
-			allpats: listdatabase.documents,
-			status: 200,
+			headpats: 0,
+			user: "User not found",
+			allpats: [],
+			status: 404,
 		}
-
-
-	} catch (e) {
-		console.error(e);
 	}
+
+
+
+	return {
+		headpats: userdata.documents[0].PatsReceived,
+		user: params.user,
+		allpats: listdatabase.documents,
+		status: 200,
+	}
+
 };
