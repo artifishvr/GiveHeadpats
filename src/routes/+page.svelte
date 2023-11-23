1413d454ba17a0b66dc32c61e81edf0eb8ecacf7
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2023-11-22 20:21:15 -0800

    it especially

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>headpats UwU</h1>
-<p>todo</p>
+<script>
+    let mostpatted = [];
+</script>
+
+<h1 class="mb-10">Give Headpats</h1>
+
+<h2>Most Headpatted</h2>
+<div class="grid grid-cols-4 gap-4">
+    {#each mostpatted as user}
+        <div class="bg-gray-200 p-4">
+            <h2>{user.name}</h2>
+            <p>Age: {user.age}</p>
+        </div>
+    {/each}
+</div>
