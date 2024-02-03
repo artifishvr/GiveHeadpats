528a7091800aa857a721d8e6431a7ae02df55047
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-02-02 20:58:01 -0800

    do a lil placeholder thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,9 @@
     import { databases } from '$lib/appwrite';
     import { Query } from 'appwrite';
     import { env } from '$env/dynamic/public';
-    import { Heading, P, Button, Footer } from 'flowbite-svelte';
+    import { Heading, P, Button, Footer, CardPlaceholder } from 'flowbite-svelte';
     import { ArrowRightOutline } from 'flowbite-svelte-icons';
+    import { browser } from '$app/environment';
 
     let mostpatted = [];
 
@@ -45,24 +46,31 @@
     <Heading tag="h1" class="mb-4 text-center" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl">Give Headpats</Heading>
     <br />
     <Heading tag="h3" class="mb-4 text-center" customSize="text-2xl font-bold md:text-2xl lg:text-2xl">Most Patted</Heading>
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max max-w-7xl mt-10 px-4 sm:px-6 mb-64">
-        {#each mostpatted as patted}
-            <div class="bg-white p-4 rounded-lg drop-shadow-sm">
-                <Heading tag="h2" class="mb-4" customSize="text-md font-bold md:text-md lg:text-md">{patted.user}</Heading>
-                <P class="mb-6 text-sm lg:text-sm dark:text-gray-400 text-center"
-                    >Has received {patted.PatsReceived} pat{patted.PatsReceived !== 1 ? 's' : ''}!</P>
-                {#if !patted.hideButton}
-                    <Button href={'/' + patted.user}>
-                        Give them a Pat!
-                        <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
-                    </Button>
-                {:else}
-                    <Button href="/account/signup">Sign up! <ArrowRightOutline class="w-3.5 h-3.5 ml-2" /></Button>
-                {/if}
-            </div>
-        {/each}
-    </div>
 
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max max-w-7xl mt-10 px-4 sm:px-6 mb-64">
+        {#if !browser}
+            <CardPlaceholder size="md" class="mt-8" />
+            <CardPlaceholder size="md" class="mt-8" />
+            <CardPlaceholder size="md" class="mt-8" />
+            <CardPlaceholder size="md" class="mt-8" />
+        {:else}
+            {#each mostpatted as patted}
+                <div class="bg-white p-4 rounded-lg drop-shadow-sm">
+                    <Heading tag="h2" class="mb-4" customSize="text-md font-bold md:text-md lg:text-md">{patted.user}</Heading>
+                    <P class="mb-6 text-sm lg:text-sm dark:text-gray-400 text-center"
+                        >Has received {patted.PatsReceived} pat{patted.PatsReceived !== 1 ? 's' : ''}!</P>
+                    {#if !patted.hideButton}
+                        <Button href={'/' + patted.user}>
+                            Give them a Pat!
+                            <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
+                        </Button>
+                    {:else}
+                        <Button href="/account/signup">Sign up! <ArrowRightOutline class="w-3.5 h-3.5 ml-2" /></Button>
+                    {/if}
+                </div>
+            {/each}
+        {/if}
+    </div>
     <Footer>
         <Heading tag="h3" class="mb-4 text-center mt-10 pt-96" customSize="text-2xl font-bold md:text-2xl lg:text-2xl"
             >Built with ❤️ by <a href="https://arti.gay" target="_blank" class="text-blue-600 dark:text-blue-500 hover:underline"
