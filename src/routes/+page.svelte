3a04b178cfdba24426182a177d19bbb321a20349
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-02-04 03:22:25 -0800

    new users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,19 +7,44 @@
     import { browser } from '$app/environment';
 
     let mostpatted = [];
+    let newusers = [];
 
-    const promise = databases.listDocuments(env.PUBLIC_HEADPATDB, env.PUBLIC_COLLECTION_USERDATA, [
+    const mostpattedResponse = databases.listDocuments(env.PUBLIC_HEADPATDB, env.PUBLIC_COLLECTION_USERDATA, [
         Query.orderDesc('PatsReceived'),
         Query.limit(4),
     ]);
 
-    promise.then(
+    mostpattedResponse.then(
         function (response) {
             mostpatted = response.documents;
 
             if (mostpatted.length < 4) {
                 for (let i = mostpatted.length; i < 4; i++) {
                     mostpatted.push({
+                        PatsReceived: 0,
+                        user: 'No one yet!',
+                        hideButton: true,
+                    });
+                }
+            }
+        },
+        function (error) {
+            console.log(error);
+        },
+    );
+
+    const newusersResponse = databases.listDocuments(env.PUBLIC_HEADPATDB, env.PUBLIC_COLLECTION_USERDATA, [
+        Query.orderDesc('$createdAt'),
+        Query.limit(4),
+    ]);
+
+    newusersResponse.then(
+        function (response) {
+            newusers = response.documents;
+
+            if (newusers.length < 4) {
+                for (let i = newusers.length; i < 4; i++) {
+                    newusers.push({
                         PatsReceived: 0,
                         user: 'No one yet!',
                         hideButton: true,
@@ -47,29 +72,41 @@
     <br />
     <Heading tag="h3" class="mb-4 text-center" customSize="text-2xl font-bold md:text-2xl lg:text-2xl">Most Patted</Heading>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max max-w-7xl mt-10 px-4 sm:px-6 mb-64">
-        {#if !browser}
-            <CardPlaceholder size="md" class="mt-8" />
-            <CardPlaceholder size="md" class="mt-8" />
-            <CardPlaceholder size="md" class="mt-8" />
-            <CardPlaceholder size="md" class="mt-8" />
-        {:else}
-            {#each mostpatted as patted}
-                <div class="bg-white p-4 rounded-lg drop-shadow-sm">
-                    <Heading tag="h2" class="mb-4" customSize="text-md font-bold md:text-md lg:text-md">{patted.user}</Heading>
-                    <P class="mb-6 text-sm lg:text-sm dark:text-gray-400 text-center"
-                        >Has received {patted.PatsReceived} pat{patted.PatsReceived !== 1 ? 's' : ''}!</P>
-                    {#if !patted.hideButton}
-                        <Button href={'/' + patted.user}>
-                            Give them a Pat!
-                            <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
-                        </Button>
-                    {:else}
-                        <Button href="/account/login">Sign up! <ArrowRightOutline class="w-3.5 h-3.5 ml-2" /></Button>
-                    {/if}
-                </div>
-            {/each}
-        {/if}
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max max-w-7xl mt-10 px-4 sm:px-6 mb-8">
+        {#each mostpatted as patted}
+            <div class="bg-white p-4 rounded-lg drop-shadow-sm">
+                <Heading tag="h2" class="mb-4" customSize="text-md font-bold md:text-md lg:text-md">{patted.user}</Heading>
+                <P class="mb-6 text-sm lg:text-sm dark:text-gray-400 text-center"
+                    >Has received {patted.PatsReceived} pat{patted.PatsReceived !== 1 ? 's' : ''}!</P>
+                {#if !patted.hideButton}
+                    <Button href={'/' + patted.user}>
+                        Give them a Pat!
+                        <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
+                    </Button>
+                {:else}
+                    <Button href="/account/login">Sign up! <ArrowRightOutline class="w-3.5 h-3.5 ml-2" /></Button>
+                {/if}
+            </div>
+        {/each}
+    </div>
+    <Heading tag="h3" class="mb-4 text-center" customSize="text-2xl font-bold md:text-2xl lg:text-2xl">New Users</Heading>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max max-w-7xl mt-10 px-4 sm:px-6 mb-24">
+        {#each newusers as patted}
+            <div class="bg-white p-4 rounded-lg drop-shadow-sm">
+                <Heading tag="h2" class="mb-4" customSize="text-md font-bold md:text-md lg:text-md">{patted.user}</Heading>
+                <P class="mb-6 text-sm lg:text-sm dark:text-gray-400 text-center"
+                    >Has received {patted.PatsReceived} pat{patted.PatsReceived !== 1 ? 's' : ''}!</P>
+                {#if !patted.hideButton}
+                    <Button href={'/' + patted.user}>
+                        Give them a Pat!
+                        <ArrowRightOutline class="w-3.5 h-3.5 ml-2" />
+                    </Button>
+                {:else}
+                    <Button href="/account/login">Sign up! <ArrowRightOutline class="w-3.5 h-3.5 ml-2" /></Button>
+                {/if}
+            </div>
+        {/each}
     </div>
     <Footer>
         <Heading tag="h3" class="mb-4 text-center mt-10 pt-96" customSize="text-2xl font-bold md:text-2xl lg:text-2xl"
