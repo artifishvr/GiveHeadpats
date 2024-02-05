7be16036476489d6481679a98416886b2ad14252
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-02-04 19:01:21 -0800

    custom domain

--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -42,6 +42,8 @@
 <Modal bind:open={popupModal} size="xs" autoclose>
     <div class="text-center">
         <CheckCircleOutline class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" />
-        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Success! Check for an email from "Arti Appwrite"</h3>
+        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+            Success! We just sent you a magic link at {email}<br />Watch for an email from "Give Headpats" (noreply@pat.arti.lol)
+        </h3>
     </div>
 </Modal>
