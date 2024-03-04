6e82b2954cfb465be073197ac4aafd5f4d8aa89a
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2024-03-03 18:48:56 -0800

    create basic error component (tbi)

---
+++ src/lib/ErrorToast.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Toast } from 'flowbite-svelte';
+      import { fly } from 'svelte/transition';
+
+    import { CloseCircleSolid } from 'flowbite-svelte-icons';
+    export let text = 'Error';
+    export let show = false;
+</script>
+
+<Toast color="red" transition={fly} params={{ x: 200 }} bind:show>
+    <svelte:fragment slot="icon">
+        <CloseCircleSolid class="w-5 h-5" />
+        <span class="sr-only">Error icon</span>
+    </svelte:fragment>
+    {text}
+</Toast>
