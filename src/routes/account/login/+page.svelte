b5e6035014b1a2f04f882f1ff733e42283f4984e
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-03-28 21:19:22 -0700

    i loooove poorly documented breaking changes

--- src/routes/account/login/+page.svelte
+++ src/routes/account/login/+page.svelte
@@ -11,7 +11,7 @@
 
     async function login(email) {
         if (!emailRegex.test(email)) return alert('Please enter a valid email address.');
-        await account.createEmailToken(ID.unique(), email, `${PUBLIC_HOST}/account/verify`);
+        await account.createMagicURLToken(ID.unique(), email, `${PUBLIC_HOST}/account/verify`);
         popupModal = true;
     }
 </script>
