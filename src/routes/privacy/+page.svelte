1435956c2c750346296beb677edb229698655016
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-04-01 12:20:36 -0700

    privacy yeahhh

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -4,7 +4,7 @@
 
 <Heading tag="h1" class="mb-4" customSize="text-2xl font-bold md:text-3xl lg:text-4xl">Privacy Policy</Heading>
 
-<P class="mb-3 md:text-xl" weight="light" color="text-gray-500 dark:text-gray-400">Last Updated: 2024-02-06</P>
+<P class="mb-3 md:text-xl" weight="light" color="text-gray-500 dark:text-gray-400">Last Updated: 2024-04-01</P>
 
 <P class="mb-3 md:text-xl" weight="light" size="lg" color="text-gray-500 dark:text-gray-400">We don't sell your information</P>
 
@@ -15,6 +15,9 @@
     - The username that you provide<br />
     - Your email provided at signup<br />
     - Your IP address to prevent spam</P>
+<P class="mb-3" weight="light" color="text-gray-500 dark:text-gray-400"
+    >We use a self-hosted plausible analytics instance to track ONLY basic visitor info, pages visited, and refferers.<br />
+    We do not track you across websites or see data on individual people.</P>
 
 <P class="mb-3" weight="light" color="text-gray-500 dark:text-gray-400">
     Additionally, we use the following third-party services: <br />
